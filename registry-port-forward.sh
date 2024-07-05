4b6513e76cb0d9757e96a24bff73ec04edde2c63
phm07 <22707808+phm07@users.noreply.github.com>
2024-07-05 15:38:16 +0200

    feat: add support for macOS (#19)
    Closes #18
    
    ---------
    
    Co-authored-by: Julian Tölle <julian.toelle@hetzner-cloud.de>
    Co-authored-by: jo <ljonas@riseup.net>

--- registry-port-forward.sh
+++ registry-port-forward.sh
@@ -2,14 +2,15 @@
 
 set -ue -o pipefail
 
-run() {
-  unit="k8s-registry-port-forward.service"
+service="k8s-registry-port-forward"
+
+run_systemd() {
   description="Port Forward for Container Registry of k8s dev environment"
 
-  systemctl --user stop "$unit" 2> /dev/null || true
+  cleanup_systemd
 
   systemd-run --user \
-    --unit="$unit" \
+    --unit="$service.service" \
     --description="$description" \
     --same-dir \
     --setenv="KUBECONFIG=$KUBECONFIG" \
@@ -17,4 +18,51 @@ run() {
     kubectl port-forward -n kube-system svc/docker-registry 30666:5000
 }
 
-run
+cleanup_systemd() {
+  systemctl --user stop "$service.service" 2> /dev/null || true
+}
+
+run_launchd() {
+  cleanup_launchd
+
+  launchctl submit \
+    -l "$service" \
+    -p "$(command -v kubectl)" \
+    -- kubectl --kubeconfig="$KUBECONFIG" port-forward -n kube-system svc/docker-registry 30666:5000
+}
+
+cleanup_launchd() {
+  launchctl remove "$service" 2> /dev/null || true
+}
+
+if command -v systemctl > /dev/null; then
+  run="run_systemd"
+  cleanup="cleanup_systemd"
+elif   command -v launchctl > /dev/null; then
+  run="run_launchd"
+  cleanup="cleanup_launchd"
+else
+  echo "No supported init system found"
+  exit 1
+fi
+
+help() {
+  cat << EOF >&2
+Usage:
+  $0 up
+  $0 down
+EOF
+  exit 1
+}
+
+case "${1:-}" in
+  "up")
+    $run
+    ;;
+  "down")
+    $cleanup
+    ;;
+  *)
+    help
+    ;;
+esac
